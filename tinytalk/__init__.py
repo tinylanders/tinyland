703c6a21f14716405fc725ae408335ea983d8fba
Ryan Prior <ryanprior@gmail.com>
2019-10-21 20:21:43 -0400

    Implements prototype Tinytalk database structure

---
+++ tinytalk/__init__.py
@@ -0,0 +1,50 @@
+from . import data
+
+def dump():
+    print("database dump:")
+    print(names)
+
+def read_tiny_talk(s):
+    print(f"result is: {s}")
+
+def init_database():
+    data.commit("aruco", "visible", {"id": 111, "x": 0, "y": 0})
+
+
+def run():
+    print("running simulation")
+    init_database()
+    data.commit("aruco", "visible", {"x": 10, "y": 1})
+    print(data.query('aruco'))
+    print("done")
+
+
+# sample tinytalk programs
+
+# syntax for data:
+# key(: val)?
+
+# syntax for a search:
+# "WHEN" adjective* tag+ (data([, ]+ data)+)? ("as" name)?
+
+## query-only visible aruco markers database (owned by aruco detection layer
+## outside tinytalk)
+# aruco = [#aruco #visible "id": 111, "x": 0, "y": 0],
+#         [#aruco #visible "id": 215, "x": 500, "y": 101]
+
+## add arucos to vessels
+# when:
+#   [#aruco id x y]
+# claim:
+#   [#vessel "id": id, "x": x, "y": y]
+
+## yields this vessels database:
+# vessels = [#vessel "id": 111, "x": 0, "y": 0],
+#           [#vessel "id": 215, "x": 500, "y": 101]
+
+## this is a valid claim
+# when:
+#   vessel = [#vessel "x": 0, "y": 0]
+# wish:
+#   vessel.affinity = vessel.affinity + 1
+#   vessel.element = "fire"
