44ab5c3cd6458e9a8cb774ae76e28c3d4bf40ad2
Aidan Holloway-Bidwell <aidanhb17@gmail.com>
2019-10-25 22:38:50 -0400

    Basic parsing with TinyTalkVisitor

--- tinytalk/data.py
+++ tinytalk/data.py
@@ -3,12 +3,14 @@ from deep_merge import merge
 
 data = defaultdict(list)
 
+
 def commit(name, *args):
     global data
     names = [name] + list(args)[:-1]
     commit_data = list(args)[-1]
     for name in names:
         data = merge(data, {name: merge({"names": names}, commit_data)})
+
 
 def query(search):
     return data[search]
--- tinytalk/grammar.py
+++ tinytalk/grammar.py
@@ -1,8 +1,10 @@
 from parsimonious.grammar import Grammar, NodeVisitor
+from parsimonious.nodes import RegexNode
+import pprint
 
 grammar = Grammar(
     r"""
-    query = "when" condition (";" condition)*
+    query = "when" condition (";" condition)* ws?
     condition = adjectives? tags data? (ws alias)?
     adjectives = (ws adjective)+
     tags = (ws tag)+
@@ -11,7 +13,7 @@ grammar = Grammar(
     ws = ~"([ \t\n,])+"
     adjective = "one" / "only" / "global" / "nearest" / "friend"
     tag = "#" name
-    datum = name (":" ws value)?
+    datum = !"as" name (":" ws value)?
     value = number / string / boolean
     boolean = "true" / "false"
     string = ~'"[ ^\"]*"'
@@ -20,28 +22,97 @@ grammar = Grammar(
     name  = ~"[a-z][a-z_-]*"
     """)
 
-class TinyTalkVisitor(NodeVisitor):
-    # def visit_query(self, node, visited_children):
-    #     """ Returns the overall output. """
-    #     output = {}
-    #     for child in visited_children:
-    #         output.update(child[0])
-    #     return output
 
-    def visit_condition(self, node, visited_children):
-        _, tags, *_ = visited_children
+def not_whitespace(o):
+    return not (isinstance(o, RegexNode) and o.expr_name == "ws")
+
+
+class Condition:
+
+    def __init__(self, *_, adjectives, tags, data, alias):
+        self.adjectives = adjectives
+        self.tags = tags
+        self.data = data
+        self.alias = alias
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(adjectives={self.adjectives}, tags={self.tags}, data={self.data}, alias={self.alias})"
+
+    def __str__(self):
+        return pprint.pformat(self)
+
+
+class TinyTalkVisitor(NodeVisitor):
+    def visit_query(self, _node, visited_children):
+        """ Returns the overall output. """
+        _when, first, rest, _ws = visited_children
+        query = [first]
+        if isinstance(rest, list):
+            query += [pair[1] for pair in rest]
+        return query
+
+    def visit_condition(self, _node, visited_children):
+        adjectives_opt, tags, data_opt, alias_opt = visited_children
+        adjectives = set(adjectives_opt[0]) if isinstance(adjectives_opt, list) else None
+        data = dict(data_opt[0]) if isinstance(data_opt, list) else None
+        alias = alias_opt[0][1] if isinstance(alias_opt, list) else None
+        cond = Condition(adjectives=adjectives,
+                         tags=set(tags),
+                         data=data,
+                         alias=alias)
+        return cond
+
+    def visit_alias(self, _node, visited_children):
+        _as, _ws, name = visited_children
+        return name.text
+
+    def visit_adjectives(self, _node, visited_children):
+        adjs = [child[1] for child in visited_children]
+        return adjs
+
+    def visit_adjective(self, _node, visited_children):
+        return visited_children[0].text
+
+    def visit_tags(self, _node, visited_children):
+        tags = [child[1] for child in visited_children]
         return tags
 
-    def visit_tags(self, node, visited_children):
-        return visited_children
-
-    def visit_tag(self, node, visited_children):
-        print("***** the node's children:")
-        print(node.children)
-        _, name = node.children
-        print("***** name.text")
-        print(name.text)
+    def visit_tag(self, _node, visited_children):
+        _pound, name = visited_children
         return name.text
+
+    def visit_data(self, _node, visited_children):
+        data = [child[1] for child in visited_children]
+        return data
+
+    def visit_datum(self, _node, visited_children):
+        _as, name, val_opt = visited_children
+        val = "ANY"
+        if isinstance(val_opt, list):
+            _ws, _colon, val = val_opt[0]
+            val = val[0]
+        return name.text, val
+
+    def visit_number(self, _node, visited_children):
+        sign_opt, integer, decimal_opt = visited_children
+        number = integer[0]
+        if isinstance(decimal_opt, list):
+            point, decimal = decimal_opt[0]
+            number += point.text
+            number += decimal[0]
+        if isinstance(sign_opt, list):
+            sign = sign_opt[0][0].text
+            number = sign + number
+        return float(number)
+
+    def visit_digit(self, node, _visited_children):
+        return node.text
+
+    def visit_boolean(self, node, _visited_children):
+        return node.text == "true"
+
+    def visit_string(self, node, _visited_children):
+        return node.text
 
     def generic_visit(self, node, visited_children):
         """ The generic visit method. """
@@ -60,6 +131,7 @@ class TinyTalkVisitor(NodeVisitor):
 # TODO commit syntax
 # TODO maybe locus declaration syntax (eg "** app #paddle")
 # TODO paramaterizable adjectives?
+# TODO fix rules to block protected namespaces?
 
 # * namespace Pong
 # when #aruco x where 0 < x < 100, y [create friend #paddle x: 100, y]
