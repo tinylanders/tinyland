f70a51c87c1e574de431ab77356b867aa12a64cc
Aidan Holloway-Bidwell <aidanhb17@gmail.com>
2019-12-08 17:25:01 -0500

    Improved and refactored Websocket Server and tinyland runtime.
    - Moved websocket server into a class and added command line arguments to websocket_server.py
    - Refactored interpreter.py
    	- Kept static methods for parsing compiled (JSON) tinyland apps as functions.
    	- Moved state-based logic into scene.TinylandScene class, such as
    		- state of scene
    		- app execution logic & triggers

--- tinytalk/interpreter.py
+++ tinytalk/interpreter.py
@@ -1,42 +1,14 @@
-"""Functions for running compiled tinytalk apps on a scene."""
-
-from collections import defaultdict, namedtuple
-from functools import reduce
+from collections import namedtuple
 import uuid
 
 from grammar import Command
 
-
-create_triggers = defaultdict(set)
-update_triggers = defaultdict(set)
-
-apps_by_id = {}  # id integer -> app JSON
-
-
-def load_app(app_json):
-    [reads, writes] = app_json
-    app_id = len(apps_by_id)
-    if app_json not in apps_by_id.values():
-        apps_by_id[app_id] = app_json
-        for match_json in reads:
-            [_command, adjectives, relation, tags, data_conditions, alias] = match_json
-            if data_conditions is None:
-                for tag in tags:
-                    create_triggers[tag].add(app_id)
-            else:
-                for tag in tags:
-                    update_triggers[tag].add(app_id) 
-
-
 # Dict mapping adjective names in tinytalk to functions that filter a list
 # of tinyland things.
-adjectives = {
-    "only": lambda l: l if len(l) == 1 else []
-}
+adjectives = {"only": lambda l: l if len(l) == 1 else []}
 
 
 def tup(**kwargs) -> namedtuple:
-    print(kwargs)
     """Convert an arbitrary number of kwargs to a namedtuple."""
     tup = namedtuple("tinyTuple", list(kwargs))
     return tup(**kwargs)
@@ -46,25 +18,6 @@ def lookup(name: str, namespace: namedtuple):
     if hasattr(namespace, str(name)):
         return getattr(namespace, name)
     return None
-
-
-def create(id_string: str, thing: dict, scene: dict):
-    tags = thing["tags"]
-    scene[id_string] = thing
-
-    apps_triggered = reduce((lambda x, y: x | y), [create_triggers[tag] for tag in tags])
-    for app_id in apps_triggered:
-        run(apps_by_id[app_id], scene, trigger_id=id_string)
-
-
-def update(id_string: str, data: dict, scene: dict):
-    thing = scene[id_string]
-    tags = thing["tags"]
-    scene[id_string].update(data)
-
-    apps_triggered = reduce((lambda x, y: x | y), [update_triggers[tag] for tag in tags])
-    for app_id in apps_triggered:
-        run(apps_by_id[app_id], scene, trigger_id=id_string)
 
 
 def condition(var_name: str, context: dict, cond_json, row: namedtuple) -> bool:
@@ -89,8 +42,9 @@ def condition(var_name: str, context: dict, cond_json, row: namedtuple) -> bool:
     elif cond_json == "ANY":
         return True
     elif cond_json[0] == Command.AND.name:
-        return (condition(var_name, context, cond_json[1], row) and
-                condition(var_name, context, cond_json[2], row))
+        return condition(var_name, context, cond_json[1], row) and condition(
+            var_name, context, cond_json[2], row
+        )
     else:
         [operator, left, right] = cond_json
 
@@ -99,7 +53,7 @@ def condition(var_name: str, context: dict, cond_json, row: namedtuple) -> bool:
             left = lookup(attribute, context[alias])
         else:
             left = lookup(left, row) or left
-        
+
         if isinstance(right, str) and "." in right:
             alias, attribute = right.split(".")
             right = lookup(attribute, context[alias])
@@ -114,6 +68,35 @@ def condition(var_name: str, context: dict, cond_json, row: namedtuple) -> bool:
             return left > right
         elif operator in ["not"]:
             return left != right
+
+
+def expression(expr_json: list, var_name: str, context: dict, row: namedtuple):
+    """Parse compiled app JSON expression and return value.
+    
+    Args:
+        expr_json: json representing visited expr node.
+        var_name: name of the variable set to this expression.
+        context: dict for looking up aliases and names used in expression.
+        row: named tuple for looking up attributes referenced in expression.
+    """
+    if type(expr_json) not in [list, tuple]:
+        if isinstance(expr_json, str) and "." in expr_json:
+            alias, attribute = expr_json.split(".")
+            result = lookup(attribute, context[alias])
+        else:
+            result = lookup(expr_json, row) or expr_json
+        # print(f"\n\n{result}\n\n")
+        return result
+    else:
+        [operator, left, right] = expr_json
+        left = expression(left, var_name, context, row)
+        right = expression(right, var_name, context, row)
+        if operator in ["*"]:
+            return left * right
+        elif operator in ["+"]:
+            return left + right
+        elif operator in ["-"]:
+            return left - right
 
 
 def match(match_json: list, context: dict, scene: dict) -> list:
@@ -131,7 +114,7 @@ def match(match_json: list, context: dict, scene: dict) -> list:
     """
     [_command, adjectives, relation, tags, data_conditions, alias] = match_json
 
-    #TODO: match first appearance of something
+    # TODO: match first appearance of something
 
     # Filter by tags.
     matches = []
@@ -141,13 +124,13 @@ def match(match_json: list, context: dict, scene: dict) -> list:
         thing_tup = tup(**thing)
         if set(thing_tup.type.split(" ")) == set(tags):
             matches.append(thing_tup)
-    
+
     # Filter by adjectives.
     if adjectives is not None:
         for adjective in adjectives:
             matches = adjectives[adjective](matches)
 
-    #TODO: Filter by relation (requires previous match contexts?)
+    # TODO: Filter by relation (requires previous match contexts?)
 
     # Filter by data_conditions.
     if data_conditions is not None:
@@ -155,103 +138,49 @@ def match(match_json: list, context: dict, scene: dict) -> list:
             if cond[0] == Command.COND.name:
                 # This is currently necessary because condition can be "ANY".
                 cond = cond[1]
-            matches = filter(lambda row: condition(var_name, context, cond, row), matches)
+            matches = list(
+                filter(lambda row: condition(var_name, context, cond, row), matches)
+            )
 
     # The following format makes it easier for match combinations
     # to be turned into context dictionaries.
     return [(alias, m) for m in matches]
 
 
-def create_from_json(create_json: list, context: dict, scene: dict, create_id: str=None):
+def create_from_json(create_json: list, context: dict, create_id: str = None):
     """Create a new tinyland object in the scene.
     
     Args:
         create_json: json for create statement.
         context: aliases mapped to their match statements.
-        scene: "database" of all that exists in tinyland.
-            See match docstring for schema.
     """
     [_command, tags, relation, data] = create_json
 
     parsed_data = {}
     for var, value in data.items():
-        if isinstance(value, str) and "." in value:
-            alias, attribute = value.split(".")
-            parsed_data[var] = lookup(attribute, context[alias])
-        else:
-            parsed_data[var] = value
-
-    new_thing = {
-        "tags": tags,
-        "type": " ".join(tags),
-        **parsed_data
-    }
-
-    if relation:
-        new_thing[relation] = [tup.id for tup in context.values()],
+        parsed_data[var] = expression(value, var, context, ())
 
     create_id = create_id or str(uuid.uuid4())
-    create(create_id, new_thing, scene)
+
+    new_thing = {"tags": tags, "type": " ".join(tags), "id": create_id, **parsed_data}
+
+    if relation:
+        new_thing[relation] = ([tup.id for tup in context.values()],)
+
+    return create_id, new_thing
 
 
-def update_from_json(update_json: list, context: dict, scene: dict) -> dict:
+def update_from_json(update_json: list, context: dict) -> dict:
+    # print("UPDATE TRIGGERED BY APP")
     """Update the desired object in tinyland scene.
     
     Args:
         update_json: json for update statement.
         context: aliases mapped to their match statements.
-        scene: "database" of all that exists in tinyland.
-            See match docstring for schema.
     """
     [_command, alias, data] = update_json
 
     parsed_data = {}
     for var, value in data.items():
-        if isinstance(value, str) and "." in value:
-            alias, attribute = value.split(".")
-            parsed_data[var] = lookup(attribute, context[alias])
-        else:
-            parsed_data[var] = value
-    
-    update_id = context[alias].id
-    update(update_id, parsed_data, scene)
-
-
-def run(app_json: list, scene: dict, trigger_id: str=None) -> dict:
-    """Run tinyland app on the tinyland scene.
-    
-    Args:
-        app_json: json for tinyland app.
-        scene: "database" of all that exists in tinyland.
-            See match docstring for schema.
-    """
-    [reads, writes] = app_json
-
-    # We need to find all combinations of our match statements.
-    # contexts will hold one context for each time we need to run the
-    # write portion of our app.
-    contexts = [{}]
-
-    # Populate contexts.
-    for match_json in reads:
-        new_contexts = []
-        for context in contexts:
-            # For each context holding n-1 matches,
-            # find matches and create an new context with the nth match.
-            matches = match(match_json, context, scene)
-            for alias, tup in matches:
-                if tup.id not in [t.id for t in context.values()]:
-                    new_contexts.append({alias: tup, **context})
-        contexts = new_contexts
-
-    for context in contexts:
-        if trigger_id is None or trigger_id in [tup.id for tup in context.values()]:
-            for write in writes:
-                if write[0] == Command.CREATE.name:
-                    create_from_json(write, context, scene)
-                elif write[0] == Command.UPDATE.name:
-                    update_from_json(write, context, scene)
-
-    
-
-
+        parsed_data[var] = expression(value, var, context, ())
+    return context[alias].id, parsed_data
--- tinytalk/websocket_server.py
+++ tinytalk/websocket_server.py
@@ -1,5 +1,7 @@
+import argparse
 import asyncio
 import json
+import logging
 import os
 import parsimonious
 import queue
@@ -8,75 +10,117 @@ import sys
 import websockets
 
 from grammar import grammar, TinyTalkVisitor
-import interpreter
+from scene import TinylandScene
 
-BUFFER_SIZE = 1024
+
 IP_ADDRESS = "127.0.0.1"
 UDP_PORT = 8766
-WEBSOCKET_PORT = 8765
+WEBSOCKET_PORT = 1234
 APP_FILE = "app.txt"
 last_upload = -sys.maxsize
 
 visitor = TinyTalkVisitor()
-scene = {}
-q = queue.Queue(64)
+udp_q = queue.Queue(64)
 
 
 def format_scene(scene):
-    formatted_scene = {
-        "appMarkers": {},
-        "virtualObjects": {},
-    }
-    for key, val in scene.items():
-        if "marker" in val["tags"]:
-            formatted_scene["appMarkers"][key] = val
-        else:
-            formatted_scene["virtualObjects"][key] = val
-    return formatted_scene
+    for k, v in scene.items():
+        if "x" in v and "y" in v:
+            scene[k].update({"point": [v["x"], v["y"]]})
+    return {"type": "render", "payload": scene}
 
 
-async def tinyland_loop(websocket, path):
-    # global sock
-    data = q.get(True)
-    new_object = json.loads(data.decode("utf-8"))
-    
-    global last_upload
-    global scene
-    if os.path.getmtime(APP_FILE) > last_upload:
-        with open(APP_FILE, "r") as f:
+class Server:
+    def __init__(self, ip_address, port):
+        self.ip_address = ip_address
+        self.port = port
+        self.scene = TinylandScene()
+
+    def reload_apps(self, filepath):
+        with open(filepath, "r") as f:
             apps = f.read().split("\n\n")
             for app in apps:
                 try:
-                    app_json = visitor.visit(grammar.parse(app))
+                    app_json = visitor.visit(grammar.parse(app.strip()))
                 except parsimonious.exceptions.ParseError as e:
-                    #TODO: add bad app error handling
-                    pass
+                    logging.error(f"Could not parse app {app_json}: {e}")
                 else:
-                    print(f"Loading app {app_json}")
-                    interpreter.load_app(app_json)
+                    logging.info(f"Loading app {app_json} from {filepath}")
+                    self.scene.load_app(app_json)
 
-        last_upload = os.path.getmtime(APP_FILE)
+    def start(self):
+        logging.info(f"Starting WebSocket Server on {self.ip_address}:{self.port}")
+        return websockets.serve(self.tinyland_loop, self.ip_address, self.port)
 
-    if new_object["id"] in scene:
-        interpreter.update(new_object["id"], new_object, scene)
-    else:
-        interpreter.create(new_object["id"], new_object, scene)
+    async def tinyland_loop(self, websocket, path):
+        while True:
+            global last_upload
 
-    await websocket.send(json.dumps(format_scene(scene)))
+            # See if we've gotten an update over UDP
+            if not udp_q.empty():
+                data = udp_q.get(True)
+                new_object = json.loads(data.decode("utf-8"))
+                if new_object["id"] in self.scene.scene:
+                    self.scene.update(new_object["id"], new_object)
+                else:
+                    self.scene.create(new_object["id"], new_object)
+
+            if os.path.getmtime(APP_FILE) > last_upload:
+                self.reload_apps(APP_FILE)
+                last_upload = os.path.getmtime(APP_FILE)
+
+            if self.scene.execute_loop():
+                await websocket.send(json.dumps(format_scene(self.scene.scene)))
+            await asyncio.sleep(0.01)
 
 
-async def udp_listener():
+async def udp_listener(ip_address, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((IP_ADDRESS, UDP_PORT))
+    logging.info(f"Started UDP listener on {ip_address}:{port}")
+    sock.settimeout(0.01)
+    sock.bind((ip_address, port))
     while True:
-        data, _addr = sock.recvfrom(1024)
-        print(data)
-        q.put(data, True)
-        await asyncio.sleep(0.5)
+        try:
+            data, _addr = sock.recvfrom(1024)
+        except socket.timeout:
+            await asyncio.sleep(0.01)
+        else:
+            logging.debug(f"UDP received: {data}")
+            udp_q.put(data, True)
+            await asyncio.sleep(0.01)
 
 
-start_server = websockets.serve(tinyland_loop, IP_ADDRESS, WEBSOCKET_PORT)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--ip_address", default=IP_ADDRESS, type=str, help="IP address to run on."
+    )
+    parser.add_argument(
+        "-p",
+        "--websocket_port",
+        default=WEBSOCKET_PORT,
+        type=int,
+        help="Port to send websocket packets to.",
+    )
+    parser.add_argument(
+        "-u",
+        "--udp_port",
+        default=UDP_PORT,
+        type=int,
+        help="Port to receive UDP messages from.",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="Verbosity level."
+    )
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().create_task(udp_listener())
-asyncio.get_event_loop().run_forever()
+    args, unknown = parser.parse_known_args(sys.argv[1:])
+
+    if args.verbose == 1:
+        logging.getLogger().setLevel(logging.INFO)
+    elif args.verbose > 1:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    ws = Server(args.ip_address, args.websocket_port)
+    asyncio.get_event_loop().run_until_complete(ws.start())
+    asyncio.get_event_loop().create_task(udp_listener(args.ip_address, args.udp_port))
+    asyncio.get_event_loop().run_forever()
